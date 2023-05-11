30fe9ba3e91eb69d1dc042ce8040074dae08f560
POO <oscar.rvalencia@hotmail.com>
2023-05-10 20:38:42 -0500

    solución a los ejercicios

---
+++ hello.js
@@ -0,0 +1,11 @@
+// escribe la función hola acá
+
+function hola (nombre) {
+    let saludo = "Hola " + nombre + "!"
+    return saludo
+}
+
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
---
+++ sum.js
@@ -0,0 +1,11 @@
+// escribe la función suma acá
+
+function suma (numero) {
+    let resultado = numero * (numero + 1) / 2
+    return resultado
+}
+
+// código de prueba
+console.log(suma(4)) // 1 + 2 + 3 + 4 = 10
+console.log(suma(10)) // 55
+console.log(suma(15)) // 120
